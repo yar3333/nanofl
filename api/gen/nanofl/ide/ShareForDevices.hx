377b6f0f62f5fb1feb1989f18db1c850c7b3e518
Yaroslav Sivakov <yar3333@gmail.com>
2024-02-21 01:59:29 +0300

    - ShareForDevices

--- api/gen/nanofl/ide/ShareForDevices.hx
+++
@@ -1,7 +0,0 @@
-package nanofl.ide;
-
-extern class ShareForDevices extends nanofl.ide.InjectContainer {
-	function new():Void;
-	function sendToService(dir:String):js.lib.Promise<{ public var statusCode(default, default) : Int; }>;
-	function generateNewLinkID():Void;
-}
