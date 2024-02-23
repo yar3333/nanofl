9ce4a0ed0f69e189b0669b1ef4edf9cee329548e
Yaroslav Sivakov <yar3333@gmail.com>
2024-02-23 15:17:41 +0300

    * minors++++

--- plugins/api/test/ApiTest.hx
+++
@@ -1,8 +0,0 @@
-import nanofl.ide.Application;
-
-class ApiTest
-{
-	static function main()
-	{
-	}
-}
---
+++ plugins/api/test/Main.hx
@@ -0,0 +1,13 @@
+//import nanofl.ide.Application;
+import nanofl.ide.plugins.ILoaderPlugin;
+import nanofl.ide.plugins.IImporterPlugin;
+import nanofl.ide.plugins.IExporterPlugin;
+import nanofl.ide.plugins.ILoaderPlugin;
+import nanofl.engine.plugins.IFilterPlugin;
+
+class Main
+{
+	static function main()
+	{
+	}
+}
