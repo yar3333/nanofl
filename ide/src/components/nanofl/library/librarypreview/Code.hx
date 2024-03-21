8b8180729a1a8f4dd6e125831325f9b9a2cd707a
Yaroslav Sivakov <yar3333@gmail.com>
2024-03-21 14:49:46 +0300

    * video preview in library bugfix

--- ide/src/components/nanofl/library/librarypreview/Code.hx
+++ ide/src/components/nanofl/library/librarypreview/Code.hx
@@ -1,16 +1,18 @@
 package components.nanofl.library.librarypreview;
 
+import nanofl.engine.AdvancableDisplayObject;
 import js.html.CanvasElement;
 import js.html.Audio;
-import nanofl.DisplayObjectTools;
 import nanofl.Stage;
 import nanofl.TextField;
 import nanofl.TextRun;
+import nanofl.DisplayObjectTools;
 import nanofl.engine.geom.Edges;
 import nanofl.engine.geom.Polygon;
+import nanofl.engine.ILibraryItem;
+import nanofl.engine.LibraryItemType;
 import nanofl.engine.libraryitems.FontItem;
 import nanofl.engine.libraryitems.InstancableItem;
-import nanofl.engine.ILibraryItem;
 import nanofl.engine.libraryitems.SoundItem;
 import nanofl.ide.Application;
 import nanofl.ide.Globals;
@@ -63,126 +65,131 @@ class Code extends wquery.Component
         stage.framerate = app.document?.properties.framerate ?? 0;
 		stage.removeAllChildren();
 		stage.clear();
+
+        switch (item?.type)
+        {
+            case LibraryItemType.bitmap:
+            case LibraryItemType.movieclip:
+            case LibraryItemType.mesh:
+            case LibraryItemType.video:
+                showInstancableItem((cast item : InstancableItem));
+            
+            case LibraryItemType.font:
+                showFontItem((cast item : FontItem));
+
+            case LibraryItemType.sound:
+                showSoundItem((cast item : SoundItem));
+            
+            case LibraryItemType.folder:
+            case null:
+                template().canvas.hide();
+                template().sound.hide();
+        }
+
+        updateStage();
+	}
+
+    function showInstancableItem(item:InstancableItem)
+    {
+        template().canvas.css("background-color", app.document != null ? app.document.properties.backgroundColor : "white");
+        template().canvas.show();
+        template().sound.hide();
+        
+        final instance = item.newInstance();
+        if (instance != null)
+        {
+            final obj = instance.createDisplayObject();
+            if (Std.isOfType(obj, AdvancableDisplayObject)) (cast obj:AdvancableDisplayObject).advanceTo(0, stage.framerate);
+            stage.addChild(obj);
+            final bounds = DisplayObjectTools.getOuterBounds(obj);
+            if (bounds != null)
+            {
+                final k = Math.min(canvas.width / bounds.width, canvas.height / bounds.height);
+                obj.scaleX = obj.scaleY = k;
+                obj.x = (canvas.width - bounds.width * k) / 2 - bounds.x * k;
+                obj.y = (canvas.height - bounds.height * k) / 2 - bounds.y * k;
+            }
+        }
+
+        stage.waitLoading().then(_ -> updateStage());
+    }
+
+    function showFontItem(item:FontItem)
+    {
+        template().canvas.css("background-color", "white");
+        template().canvas.show();
+        template().sound.hide();
+        
+        final font = item.toFont();
+        
+        final text = new TextField(0, 0, false, false, false, font.variants.mapi((i, v) -> TextRun.create
+        (
+            "Abc Эюя" + (i + 1 < font.variants.length ? "\n":""),
+            "#000000",
+            font.family,
+            v.style,
+            30,
+            "left",
+            0,
+            "#000000",
+            true,
+            0,
+            0
+        )).array());
+        
+        text.update();
+        
+        final bounds = DisplayObjectTools.getInnerBounds(text);
+        if (bounds != null)
+        {
+            text.x = (canvas.width  - bounds.width ) / 2;
+            text.y = (canvas.height - bounds.height) / 2;
+            stage.addChild(text);
+        }
+    }
+
+    function showSoundItem(item:SoundItem)
+    {
+        template().canvas.hide();
+        template().sound.show();
+        template().sound.find(">i").attr("class", soundPlaying != item.namePath ? "icon-play" : "icon-pause");
+    }
+	
+	function sound_click(_)
+	{
+		if (!Std.isOfType(item, SoundItem)) return;
 		
-		if (item != null)
-		{
-			if (Std.isOfType(item, InstancableItem))
-			{
-				template().canvas.css("background-color", app.document != null ? app.document.properties.backgroundColor : "white");
-				template().canvas.show();
-				template().sound.hide();
-				
-                var instance = (cast item:InstancableItem).newInstance();
-                if (instance != null)
-                {
-                    var obj = instance.createDisplayObject();
-                    stage.addChild(obj);
-                    var bounds = DisplayObjectTools.getOuterBounds(obj);
-                    if (bounds != null)
-                    {
-                        var k = Math.min(canvas.width / bounds.width, canvas.height / bounds.height);
-                        obj.scaleX = obj.scaleY = k;
-                        obj.x = (canvas.width - bounds.width * k) / 2 - bounds.x * k;
-                        obj.y = (canvas.height - bounds.height * k) / 2 - bounds.y * k;
-                    }
-                }
-			}
-			else
-			if (Std.isOfType(item, FontItem))
-			{
-				template().canvas.css("background-color", "white");
-				template().canvas.show();
-				template().sound.hide();
-				
-				var font = (cast item:FontItem).toFont();
-				
-				var text = new TextField(0, 0, false, false, false, font.variants.mapi(function(i, v)
-				{
-					return TextRun.create
-					(
-						"Abc Эюя" + (i + 1 < font.variants.length ? "\n":""),
-						"#000000",
-						font.family,
-						v.style,
-						30,
-						"left",
-						0,
-						"#000000",
-						true,
-						0,
-						0
-					);
-				}
-				).array());
-				
-				text.update();
-				
-				var bounds = DisplayObjectTools.getInnerBounds(text);
-				if (bounds != null)
-				{
-					text.x = (canvas.width  - bounds.width ) / 2;
-					text.y = (canvas.height - bounds.height) / 2;
-					stage.addChild(text);
-				}
-			}
-			else
-			if (Std.isOfType(item, SoundItem))
-			{
-				template().canvas.hide();
-				template().sound.show();
-				template().sound.find(">i").attr("class", soundPlaying != item.namePath ? "icon-play" : "icon-pause");
-			}
-			else
-			{
-				template().canvas.hide();
-				template().sound.hide();
-			}
-		}
-		else
-		{
-			template().canvas.hide();
-			template().sound.hide();
-		}
-		
-		Edges.showSelection = false;
+        if (soundPlaying == item.namePath) { stopSound(); return; }
+        
+        soundPlaying = item.namePath;
+        final sound : SoundItem = cast item;
+        sound.preload().then(_ ->
+        {
+            if (soundPlaying == item.namePath)
+            {
+                audioPlaying = sound.play();
+                audioPlaying.addEventListener("ended", () -> stopSound());     
+            }
+        });
+
+        update();
+	}
+
+    function updateStage()
+    {
+ 		Edges.showSelection = false;
 		Polygon.showSelection = false;
 		
 		stage.update();
 		
 		Edges.showSelection = true;
 		Polygon.showSelection = true;
-	}
-	
-	function sound_click(_)
-	{
-		if (Std.isOfType(item, SoundItem))
-		{
-			if (soundPlaying != item.namePath)
-			{
-                soundPlaying = item.namePath;
-                final sound : SoundItem = cast item;
-                sound.preload().then(_ ->
-                {
-                    if (soundPlaying == item.namePath)
-                    {
-                        audioPlaying = sound.play();
-                        audioPlaying.addEventListener("ended", () -> 
-                        {
-                            if (audioPlaying != null) { audioPlaying.pause(); audioPlaying = null; }
-                            soundPlaying = null;
-                            update();
-                        });     
-                    }
-                });
+   }
 
-                update();
-			}
-			else
-			{
-				if (audioPlaying != null) { audioPlaying.pause(); audioPlaying = null; }
-                soundPlaying = null;
-				update();
-			}
-		}
-	}
+    function stopSound()
+    {
+        if (audioPlaying != null) { audioPlaying.pause(); audioPlaying = null; }
+        soundPlaying = null;
+        update();
+    }
 }
